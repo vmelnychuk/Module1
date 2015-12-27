e312d9e91669a9c5f0e1179cd3826ae6e1727ddb
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2015-12-27 23:36:00 +0300

    add freemarker

--- src/main/java/servlet/FrontEnd.java
+++ src/main/java/servlet/FrontEnd.java
@@ -7,8 +7,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class FrontEnd extends HttpServlet {
+    private String login = "";
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
          resp.getWriter().append("hello");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String loginForm = req.getParameter("login");
+        String passwordForm = req.getParameter("password");
     }
 }
