56c492628440e73b11bc5698e390bdbf735bcc50
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2015-12-27 22:35:47 +0300

    refactor exception handling

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,22 +7,21 @@ import servlet.FrontEnd;
 
 public class Main {
     public static final int PORT = 8080;
+    public static final String AUTH_FORM = "/authform";
     public static void main(String[] args) {
         FrontEnd frontEnd = new FrontEnd();
 
         Server server = new Server(PORT);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
-        context.addServlet(new ServletHolder(frontEnd), "/authform");
+        context.addServlet(new ServletHolder(frontEnd), AUTH_FORM);
 
         try {
             server.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
             server.join();
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
